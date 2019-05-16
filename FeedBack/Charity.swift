45f7bae55d75c9dd893b918bf8c337ad275d6b00
Julian <mcjulian43@gmail.com>
2019-05-16 14:05:49 +0200

    Refactored enums

--- FeedBack/Charity.swift
+++ FeedBack/Charity.swift
@@ -10,8 +10,11 @@ enum CharityCategory: String{
     case others = "others"
 }
 
-enum CharityImpactType{
-    case childTreated, netFounded, ntdTreated, none
+enum CharityImpactType: String{
+    case childTreated = "treatchild"
+    case netFounded = "malarianets"
+    case ntdTreated = "treatntd"
+    case none = "none"
 }
 
 
@@ -19,14 +22,14 @@ class Charity: NSObject{
     var cid: String
     var name: String
     var category: CharityCategory
-    var impactCount: Float
+    var impactPerDollar: Float
     var impactType: CharityImpactType
     var website: String
     
     init(cid: String, name: String, impactCount: Float, impactType: CharityImpactType, website: String, category: CharityCategory){
         self.cid = cid
         self.name = name
-        self.impactCount = impactCount
+        self.impactPerDollar = impactCount
         self.impactType = impactType
         self.website = website
         self.category = category
@@ -37,41 +40,16 @@ class Charity: NSObject{
         guard let charityDb = snapshot.value as? [String:Any] else { return nil }
         guard let name = charityDb[charityNameChildPath] as? String else { return nil }
         guard let categoryAsString = charityDb[charityCategoryChildPath] as? String else { return nil }
-        guard let impactCount = charityDb[charityImpactPerDollarChildPath] as? String else { return nil }
+        guard let impactPerDollar = charityDb[charityImpactPerDollarChildPath] as? String else { return nil }
         guard let impactTypeAsString = charityDb[charityImpactTypeChildPath] as? String else { return nil }
         guard let website = charityDb[charityWebsiteChildPath] as? String else { return nil }
         
-        func getCategory(_ categoryString: String) -> CharityCategory{
-            switch(categoryString){
-            case "health":
-                return CharityCategory.health
-            case "enviroment":
-                return CharityCategory.enviromental
-            case "animals":
-                return CharityCategory.animals
-            default:
-                return CharityCategory.others
-            }
-        }
-        
-        func getType(_ string: String) -> CharityImpactType{
-            switch(string){
-            case "childtreated":
-                return CharityImpactType.childTreated
-            case "netfounded":
-                return CharityImpactType.netFounded
-            case "ntdtreated":
-                return CharityImpactType.ntdTreated
-            default:
-                return CharityImpactType.none
-            }
-        }
         
         self.cid = cid
         self.name = name
-        self.category = getCategory(categoryAsString)
-        self.impactCount = Float(impactCount)!
-        self.impactType = getType(impactTypeAsString)
+        self.category = CharityCategory.init(rawValue: categoryAsString) ?? CharityCategory.others
+        self.impactPerDollar = Float(impactPerDollar)!
+        self.impactType = CharityImpactType.init(rawValue: impactTypeAsString) ?? CharityImpactType.none
         self.website = website
         
     }
--- FeedBack/DonationViewController.swift
+++ FeedBack/DonationViewController.swift
@@ -1,14 +1,33 @@
 import UIKit
 
-class DonationViewController: UIViewController {
+class DonationViewController: UIViewController, UITextFieldDelegate{
 
     var charity: Charity?
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var moneyAmountTextField: UITextField!
+    @IBOutlet weak var impactAmountTextLabel: UILabel!
+    @IBOutlet weak var impactView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        moneyAmountTextField.delegate = self
+        moneyAmountTextField.keyboardType = .numberPad
+        
         if(charity != nil){
-        titleLabel.text = charity!.name
+            titleLabel.text = charity!.name
         }
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        impactView.isHidden = false
+        
+        let amountEntered = Float(textField.text!)
+        let valuePerDollar = charity?.impactPerDollar
+        
+        impactAmountTextLabel.text =  String(amountEntered!*valuePerDollar!)
+        
+        let invalidCharacters = CharacterSet(charactersIn: "0123456789.").inverted
+        return string.rangeOfCharacter(from: invalidCharacters) == nil
+    }
+    
 }
